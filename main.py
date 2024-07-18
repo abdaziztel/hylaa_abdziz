6be55717ad59ca25ce2683f4b4eb3401733b050f
abdaziztel <Mabdulazizff1995@gmail.com>
2024-07-18 19:13:50 +0300

    Update main.py

--- main.py
+++ main.py
@@ -75,8 +75,7 @@ def send_product_data_to_telegram(product_name, product_status, image_url, produ
     telegram_api_url = f"https://api.telegram.org/bot{bot_token}/sendPhoto"
     message_text = f"Product Name: {product_name}\nProduct Status: {product_status}"
     
-    # Exclude inline keyboard if the product status is "متوفر" or "سيتم توفيرها في المخزون قريباً"
-    if product_status  in ["متوفر"]:
+    if product_status == "متوفر":
         reply_markup = {
             "inline_keyboard": [
                 [{"text": "عرض المنتج", "url": product_link}],
@@ -85,19 +84,20 @@ def send_product_data_to_telegram(product_name, product_status, image_url, produ
                 [{"text": "الانتقال إلى رابط الدفع النهائي", "url": "https://www.dzrt.com/ar/onestepcheckout.html"}]
             ]
         }
-        params = {
-            "chat_id": chat_id,
-            "photo": image_url,
-            "caption": message_text,
-            "reply_markup": json.dumps(reply_markup)
-        }
     else:
-        params = {
-            "chat_id": chat_id,
-            "photo": image_url,
-            "caption": message_text
+        reply_markup = {
+            "inline_keyboard": [
+                [{"text": "تسجيل الدخول", "url": "https://www.dzrt.com/ar/customer/account/login/"}]
+            ]
         }
-
+    
+    params = {
+        "chat_id": chat_id,
+        "photo": image_url,
+        "caption": message_text,
+        "reply_markup": json.dumps(reply_markup)
+    }
+    
     response = requests.post(telegram_api_url, params=params)
     if response.status_code == 200:
         print(f"Product data sent successfully for {product_name}")
